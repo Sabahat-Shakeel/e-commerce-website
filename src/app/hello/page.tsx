075ed6a966ba4151f54c9d6a8f9e06da99911b7f
syeda <sbtshakeel61@gmail.com>
2024-09-23 11:33:52 +0500

    updated some tailwind

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -2,32 +2,30 @@ import React from "react";
 const Main = () => {
   return (
     <div>
-      <main className="bg-[#d8e2dc] content-center h-72 lg:h-screen">
+      <main className="bg-[#d8e2dc] content-center h-screen lg:h-screen">
        
-        <h3 className="text-gray-400 -translate-y-16 text-[6px] font-serif underline cursor-pointer hover:underline-offset-2 lg:text-xs lg:-translate-y-36 lg:pb-2 ml-[2px] ">
-          Email : sbt01@gmail.com
-        </h3>
+      
 
-        {/* <img className="h-40" src="/sbt.png" alt="" /> */}
+       
         <div className="">
-        <h1 className="flex justify-center font-[Poppins] font-extralight text-2xl  lg:text-7xl ">
+        <h1 className="flex justify-center font-[Poppins] font-normal text-4xl  lg:text-7xl ">
           LONG WEEKEND{" "}
         </h1>
-        <h1 className=" font-extrabold flex justify-center text-2xl lg:text-7xl">
-          SALE 30 - 60 % OFF{" "}
+        <h1 className=" font-extrabold flex justify-center pt-5 text-3xl lg:text-7xl">
+          SALE  <span className="text-cyan-900 ml-2"> 30 - 60</span>  % OFF{" "}
         </h1>
 
-        <h1 className="flex justify-center font-extrabold text-2xl lg:text-7xl">
-          EXTRA 20% OFF{" "}
+        <h1 className="flex justify-center font-extrabold text-4xl pt-5 lg:text-7xl">
+          EXTRA <span className="text-cyan-900 ml-2">20%</span> OFF{" "}
         </h1>
         </div>
-        <p className="flex justify-center font-light text-[6px] lg:text-base lg:pt-2 ">SITEWIDE</p>
-        <p className="flex justify-center font-light text-[6px] lg:text-base lg:pb-4  ">YOUR ORDER</p>
-        <div className="text-center">
-          <button className="inline-block hover:scale-105 text-[10px] px-2  ml-2 shadow-yellow-400 bg-[#001d3d] rounded-br-3xl rounded-t-3xl text-white font-medium hover:opacity-95 lg:text-base lg:px-4 lg:py-2">
+        <p className="flex justify-center font-medium pt-2 text-sm lg:text-base lg:pt-2 ">SITEWIDE</p>
+        <p className="flex justify-center font-medium text-sm lg:text-base lg:pb-4  ">YOUR ORDER</p>
+        <div className="text-center pt-2">
+          <button className="inline-block hover:scale-105 text-sm px-2 ml-2 shadow-yellow-400 bg-[#001d3d] sm:rounded-br-3xl sm:rounded-t-3xl text-white font-medium hover:opacity-95 lg:text-base lg:px-4 lg:py-2 rounded  ">
             Login
           </button>
-          <button className="inline-block hover:scale-105 text-[10px] px-1  ml-2 shadow-yellow-400 bg-[#001d3d] rounded-br-3xl rounded-t-3xl text-white font-medium hover:opacity-95 lg:text-[15px] lg:px-4 lg:py-2  ">
+          <button className="inline-block hover:scale-105 text-sm px-2  ml-2 shadow-yellow-400 bg-[#001d3d] sm:rounded-br-3xl sm:rounded-t-3xl text-white font-medium hover:opacity-95 lg:text-[15px] lg:px-4 lg:py-2 rounded  ">
             Sign Up
           </button>
           </div>
--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -8,13 +8,13 @@ function Navbar() {
       {/* <h1 className="font-extrabold text-pink-600 text-opacity-80 py-2 ">sbt-brand</h1> */}
       <div className="marquee-container  bg-[#0f4c5c]">
         <div className="marquee-content  ">
-          <span className="sm:text-base  text-[9px] font-extralight font-sans text-neutral-100 hover:text-gray-200 ">
+          <span className="sm:text-base  text-sm font-extralight font-sans text-neutral-100 hover:text-gray-200 ">
             Sbt -E- Commerce-Website
           </span>
         </div>
       </div>
       <div className="bg-black  ">
-        <nav className=" text-xs   ">
+        <nav className=" text-base   ">
           <ul className="flex lg:justify-center ml-2 gap-2 sm:text-base sm:mr-40 sm:pt-2 sm:flex lg:flex xl:flex 2xl:flex   ">
             <li className=" text-neutral-500 dark:text-neutral-300 hover:text-gray-100">
               <Link href="/">Home</Link>
@@ -33,7 +33,7 @@ function Navbar() {
             </li>
           </ul>
 
-          <div className="flex ml-11 justify-center sm:ml-20 sm:flex sm:justify-end ">
+          <div className="flex  justify-center sm:ml-20 sm:flex sm:justify-end ">
             <input
               className="rounded-lg m-2 sm:placeholder:px-14 sm:py-[10px] sm:m-2 lg:px-11 xl:px-11 sm:rounded-xl bg-neutral-200 hover:bg-gray-100 dark:text-neutral-900  placeholder:text-[12px] outline-none placeholder-slate-400 text-center sm:-translate-y-3  sm:ml-44"
               type="search"
