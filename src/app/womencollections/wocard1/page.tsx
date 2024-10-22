6b9a40da3d3379b763ed1ea8ef60860b16686d94
Sabahat Shakeel <159911882+Sabahat-Shakeel@users.noreply.github.com>
2024-10-22 17:15:56 +0500

    Update

--- src/app/womencollections/wocard1/page.tsx
+++ src/app/womencollections/wocard1/page.tsx
@@ -20,7 +20,7 @@ export default function Women1() {
         </Link>
         <Link href="/makeup">
           <p className="text-base  text-black mt-4 mb-2 dark:text-neutral-200">
-           New Arrival collections
+           Women Arrival collections
           </p>
         </Link>
         <p className="text-xs text-neutral-600 dark:text-neutral-400">
